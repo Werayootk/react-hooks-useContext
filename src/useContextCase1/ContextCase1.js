46f476d7aec0c1111f7f9ddfe0f0962c3d57d9b5
Werayootk <werayoot5800@gmail.com>
2021-08-13 17:02:19 +0700

    Done

--- src/useContextCase1/ContextCase1.js
+++ src/useContextCase1/ContextCase1.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from "react";
+import Login from "./Login";
+import User from "./User";
+
+export const AppContext = createContext(null);
 
 const ContextCase1 = () => {
-
-    return <div>
-
-    </div>
-};
+    const [username, setUsername] = useState("");
+    
+    return (
+        <AppContext.Provider value={{ username, setUsername }}>
+            <Login />
+            <User />
+        </AppContext.Provider>
+    );
+}
 
 export default ContextCase1;
